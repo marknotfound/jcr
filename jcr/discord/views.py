93544336842c807aac021ee2815500de908048cb
mark evans <mark@meltdownlabs.com>
2023-05-04 09:56:55 -0400

    notify discord when new vol opportunities come in (#15)
    * notify discord when new vol opportunities come in
    
    * capitalize
    
    * update test to include webhook
    
    * add event

--- jcr/discord/views.py
+++ jcr/discord/views.py
@@ -1,13 +1,22 @@
 from http import HTTPStatus
+import requests
 from rest_framework import viewsets, response, mixins
+from django.conf import settings
 from . import serializers, models
+from .messages import MessageGenerator
 
 
 class VolunteerOpportunityWebhook(viewsets.ViewSet):
     def create(self, request):
         serializer = serializers.VolunteerOpportunityWebhookPayload(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        instances = serializer.save()
+
+        message = MessageGenerator.generate_volunteer_opportunities_message(instances)
+
+        if message:
+            SUPRESS_EMBEDS = 1 << 2
+            requests.post(settings.NYRR_WEBHOOK, { "content": message, "flags": SUPRESS_EMBEDS })
 
         return response.Response(None, status=HTTPStatus.NO_CONTENT)
 
